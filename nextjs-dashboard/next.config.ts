4b60e228c68d092c3b8d58a86d64c505e78c9de4
Rafiat <bolajirafiat@gmail.com>
2025-02-07 17:14:09 -0500

    completed (6-11)

--- nextjs-dashboard/next.config.ts
+++ nextjs-dashboard/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next';
+import * as NextCanary from 'next-canary';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // experimental: {
+  //   ppr: 'incremental'
+  // }
 };
 
 export default nextConfig;
